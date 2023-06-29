a22a0c7ff2fd479be45632bd2cbe8a956558bf08
Thomas Ricci <git@thomasricci.dev>
2023-06-29 13:37:20 -0400

    no more vts!!! its too hard in htmx :(
    also need to do more work on client-side validation

--- web2/js/main.js
+++ web2/js/main.js
@@ -3,22 +3,34 @@ document.body.addEventListener("htmx:beforeRequest", (evt) => {
   const pth = evt.detail.pathInfo.finalRequestPath;
   switch(pth) {
     case "/hx/tts.html": {
-      document.getElementById("vts").classList.remove("active-tab");
+      // document.getElementById("vts").classList.remove("active-tab");
       document.getElementById("ats").classList.remove("active-tab");
       document.getElementById("tts").classList.add("active-tab");
       break;
     }
-    case "/hx/vts.html": {
-      document.getElementById("tts").classList.remove("active-tab");
-      document.getElementById("ats").classList.remove("active-tab");
-      document.getElementById("vts").classList.add("active-tab");
-      break;
-    }
     case "/hx/ats.html": {
       document.getElementById("tts").classList.remove("active-tab");
-      document.getElementById("vts").classList.remove("active-tab");
+      // document.getElementById("vts").classList.remove("active-tab");
       document.getElementById("ats").classList.add("active-tab");
       break;
     }
   }
 });
+
+document.body.addEventListener("htmx:afterRequest", (evt) => {
+  if(evt.detail.pathInfo.finalRequestPath === "/hx/ats.html") {
+    const elem = document.querySelector("#input > form > label > input[type=file]");
+    elem.checkValidity = () => {
+      console.log(this);
+      return false;
+    };
+  }
+})
+
+const mb = (size) => {
+  return size * 1048576;
+}
+
+const sec = (length) => {
+  return length * 1000;
+}
